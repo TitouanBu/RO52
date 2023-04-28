11d1f0686ae0a57313e8be6d3365b0c017ec204e
TitouanBu <titouan.burgy@laposte.net>
2023-04-28 16:30:19 +0200

    update log

--- TP3/log.py
+++ TP3/log.py
@@ -4,7 +4,7 @@ import time
 def write_log(distance,vitesse):
     # date_string = time.strftime("%d-%m-%Y")
     # time_string = time.strftime("%H:%M:%S")
-    message = "{};{}\n".format()
+    message = "{};{}\n".format(distance,vitesse)
     
     # Écrire la donnée dans le fichier txt
     with open('log.txt', 'a') as file:
